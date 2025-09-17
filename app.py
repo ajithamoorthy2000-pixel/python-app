c1df170a31b37e1b88d572a82e9a317d30745451
ajithamoorthy2000-pixel <ajithamoorthy2000@gmail.com>
2025-09-17 15:33:36 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py - Simple Python Application
+
+def greet(name):
+    return f"Hello, {name}! Welcome to GitHub experiment."
+
+if __name__ == "__main__":
+    user = input("Enter your name: ")
+    print(greet(user))
